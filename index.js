6c9d948ad2889ec64c29e6575ef92ac83bdb5798
widyafebriandaru <febriandaru77@gmail.com>
2023-06-22 10:39:33 +0700

    paginantion success

--- index.js
+++ index.js
@@ -5,19 +5,46 @@ const { append } = require("express/lib/response");
 const app = express();
 const { sequelize } = require("./models");
 const db = require("./models");
+const thumbnail = require("./models/thumbnail");
+const detailproduct = require("./models/detailproduct");
 
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors("*"));
 app.use(morgan("tiny"));
 
-app.get("/", async(req,res) => {
-    const products = await db.detailProduct.findAll();
+const itemsPerPage = 2; // Number of items (columns) to display per page
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const page = parseInt(req.query.page || 1, 10); // Get the page number from the query parameter, default to 1 if not provided
+
+    // Calculate the offset based on the page number and items per page
+    const offset = (page - 1) * itemsPerPage;
+
+    // Retrieve the desired columns for the page based on the category
+    const products = await db.detailProduct.findAll({
+      attributes: ['category'],
+      where: {
+        category: 'Shirt',
+      },
+      order: [['id', 'ASC']],
+      offset,
+      limit: itemsPerPage,
+    });
 
     return res.status(200).json({
-        data: products,
-    });
+      data: products,
+    })
+ 
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
+
+
+
 // app.get("/Atest", async(req,res) => {
 //     const users = await db.User.findOne();
 
@@ -25,7 +52,6 @@ app.get("/", async(req,res) => {
 //         data: users,
 //     });
 // });
-
 
 app.listen(3001, () => {
     console.clear();
