60edeb36e20fb15c98627e66edecbd307b4a86b5
widyafebriandaru <febriandaru77@gmail.com>
2023-06-22 20:08:40 +0700

    SQL query success

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const db = require("./models");
 const thumbnail = require("./models/thumbnail");
 const detailproduct = require("./models/detailproduct");
 
+
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors("*"));
@@ -21,17 +22,25 @@ app.get('/api/products', async (req, res) => {
 
     // Calculate the offset based on the page number and items per page
     const offset = (page - 1) * itemsPerPage;
-
+    // http://localhost:3001/api/products?page=1
     // Retrieve the desired columns for the page based on the category
-    const products = await db.detailProduct.findAll({
-      attributes: ['category'],
-      where: {
-        category: 'Shirt',
-      },
-      order: [['id', 'ASC']],
-      offset,
-      limit: itemsPerPage,
-    });
+
+
+
+    // const products = await db.detailProduct.findAll({
+    //   attributes: ['detailName','category'],
+    //   where: {
+    //     category: 'T-Shirt',
+    //   },
+    //   order: [['id', 'ASC']],
+    //   offset,
+    //   limit: itemsPerPage,
+      
+    // });
+
+    const products = await sequelize.query(
+      "SELECT p.*, t.* FROM detailProducts AS p JOIN thumbnails AS t ON p.thumbnail = t.id;"
+    );
 
     return res.status(200).json({
       data: products,
